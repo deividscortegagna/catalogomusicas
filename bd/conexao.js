1f29db7f93b5ac7de05e15b6237196ad1ab3c8b9
felipetriches20 <felipetriches20@gmail.com>
2020-09-24 23:39:43 -0300

    Novo Usuario - Index

--- bd/conexao.js
+++ bd/conexao.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const conexao = new Sequelize('catalogomusicasbd', 'postgres', 'ti@123', {
-    host: 'localhost',
-    dialect: 'postgres',
-    timezone: '-03:00'
+const conexao = new Sequelize("catalogomusicasbd", "postgres", "ti@123", {
+  host: "localhost",
+  dialect: "postgres",
+  timezone: "-03:00",
 });
 
 module.exports = conexao;
--- index.js
+++ index.js
@@ -19,4 +19,35 @@ conexao.authenticate();
 app.get("/", function (req, res) {
   res.render("login", { mensagem: "" });
 });
+app.get("/index", function (req, res) {
+  res.render("index");
+});
+
+app.post("/login", function (req, res) {
+  Usuarios.findOne({ where: { login: req.body.login } }).then(function (
+    usuario
+  ) {
+    if (usuario != undefined) {
+      if (usuario.senha == req.body.senha) {
+        res.redirect("/index");
+      } else res.render("login", { mensagem: "Usuário ou senha inválidos" });
+    } else res.render("login", { mensagem: "Usuário ou senha inválidos" });
+  });
+});
+
+app.get("/usuarios/novo", function (req, res) {
+  res.render("usuarios");
+});
+app.get("/usuarios/cancelar", function (req, res) {
+  res.render("login", { mensagem: "" });
+});
+app.post("/usuarios/salvar", function (req, res) {
+  let nome = req.body.nome;
+  let email = req.body.login;
+  let senha = req.body.senha;
+  Usuarios.create({ nome: nome, login: email, senha: senha }).then(
+    res.render("login", { mensagem: "Usuario Cadastrado." })
+  );
+});
+
 app.listen(3000);
